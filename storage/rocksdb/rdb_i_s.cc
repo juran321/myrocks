f48013f647c954ba5be40784867ad62c1403354c
Ran Ju <juran94@uw.edu>
2018-11-12 19:55:13 +0000

    change test case to test delete_type and update_type

--- storage/rocksdb/rdb_i_s.cc
+++ storage/rocksdb/rdb_i_s.cc
@@ -1124,10 +1124,12 @@ int Rdb_fkinfo_scanner::add_table(Rdb_tbl_def *tdef){
     std::string delete_type;
     std::string update_type;
 
-    int err;
-		err = rdb_split_normalized_tablename(referenced_tablename,&referenced_db, &referenced_table, &referenced_partname);
-
-		DBUG_ASSERT(err == 0);
+    
+		int err = rdb_split_normalized_tablename(referenced_tablename,&referenced_db, &referenced_table, &referenced_partname);
+    if(err != 0){
+      DBUG_RETURN(0);
+    }
+		//DBUG_ASSERT(err == 0);
 		std::shared_ptr<const Rdb_key_def> foreign_keydef =ddl_manager_cur->safe_find(foreign_gl_index_id);
 		std::shared_ptr<const Rdb_key_def> referenced_keydef = ddl_manager_cur->safe_find(referenced_gl_index_id);
 
